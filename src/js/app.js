b236c48ddf424459e2fd8c56a5e3a9d2fc395aae
lvergaram <lvergaramena@gmail.com>
2024-02-17 18:07:11 -0300

    form y footer listo

--- src/js/app.js
+++ src/js/app.js
@@ -3,4 +3,16 @@ $(function () {
       var $nav = $(".navbar");
       $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
     });
+
+
+    $("#enviar-correo").click(function(){
+      // Mostrar una alerta con el mensaje "El correo fue enviado correctamente..."
+      alert("El correo fue enviado correctamente...");
+    });
+
+
   });
+
+
+const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
